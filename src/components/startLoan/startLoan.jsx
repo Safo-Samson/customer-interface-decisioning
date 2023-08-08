4dfe877d072160d864da174fa6d7cc4b2003efb5
emiunal <E.unal2@newcastle.ac.uk>
2023-08-08 16:36:02 +0100

    work on styling

--- src/components/startLoan/startLoan.jsx
+++ src/components/startLoan/startLoan.jsx
@@ -1,25 +1,65 @@
 import React from "react";
-import './startLoan.css'
+import './startLoan.css';
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoins } from "@fortawesome/free-solid-svg-icons";
+import { faCreditCard } from "@fortawesome/free-solid-svg-icons";
+import { faCar } from "@fortawesome/free-solid-svg-icons";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { faMoneyCheckDollar } from "@fortawesome/free-solid-svg-icons";
+import { faHandHoldingDollar } from "@fortawesome/free-solid-svg-icons";
+import { faSterlingSign } from "@fortawesome/free-solid-svg-icons";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+
 
 const StartLoan = () =>{
+
+    
+const [loanAmount, setLoan ] = useState("");
 
 
 return(<>
     <div className="applyLoan-container">
-        <h1 className="sev-loanHeader">START YOUR NEW LOAN APPLICATION</h1>
+    <div className="applyLoan-header">
+    <FontAwesomeIcon className='coinsIcon' icon={faCoins}/>
+    <h1 className="sev-loanHeader">START YOUR NEW LOAN APPLICATION</h1>
+    </div>
+    
+        
         <div className="sev-mainContent">
         <div className="decide-container">
             <p>Decide on your borrowing option:</p>
-            <button className="overDraft">Overdraft</button>
-            <button className="creditCard">Credit Card</button>
-            <button className="loan">Loan</button>
-            <button className="carFinance">Car Finance</button>
-            <button className="mortgage">Mortgage</button>
+            <button className="overDraft" ><FontAwesomeIcon icon={faHandHoldingDollar} style={{color: "#005F44"}} />Overdraft</button>
+            <button className="creditCard"> <FontAwesomeIcon icon={faCreditCard} style={{color: "#005F44"}} /> Credit Card</button>
+            <button className="loan"><FontAwesomeIcon icon={faMoneyCheckDollar} style={{color: "#005F44"}} />Loan</button>
+            <button className="carFinance"> <FontAwesomeIcon icon={faCar} style={{color: "#005F44"}} />Car Finance</button>
+            <button className="mortgage"><FontAwesomeIcon icon={faHouse} style={{color: "#005F44"}} />Mortgage</button>
+            
         </div>
         <div className="enterAmount-container">
             <p>Enter your desired amount: </p>
+            <div className="loanAmount">
+                
+                <label className="loanAmountLabel"> Loan Amount</label>
+                <div className="inputFieldLoan">
+                <FontAwesomeIcon className='poundSign' icon={faSterlingSign} />
+                <input             
+                        value={loanAmount}
+                        onChange={(e) => setLoan(e.target.value)}
+                        type = "loanAmount"
+                        placeholder=""
+                        id="loanAmount"
+                        name="loanAmount"
+                />
+                </div>
+            </div>
+            
         </div>
+        
         </div>
+        <button className="backBtn"> <FontAwesomeIcon icon={faChevronLeft} /> Back</button>
+        <button className="nextBtn">Next  <FontAwesomeIcon icon={faChevronRight} /></button>
     </div>
 
 
