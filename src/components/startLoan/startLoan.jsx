9659583fd0ed09e549287d496fa02193fa6dce54
emiunal <E.unal2@newcastle.ac.uk>
2023-08-08 12:36:36 +0100

    start the loan app page

---
+++ src/components/startLoan/startLoan.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import './startLoan.css'
+
+const StartLoan = () =>{
+
+
+return(<>
+    <div className="applyLoan-container">
+        <h1 className="sev-loanHeader">START YOUR NEW LOAN APPLICATION</h1>
+        <div className="decide-container">
+            <h4>Decide on your borrowing option:</h4>
+        </div>
+        <div className="enterAmount-container">
+            <h4>Enter your desired amount: </h4>
+        </div>
+    </div>
+
+
+</>)
+}
+
+
+export default StartLoan;
