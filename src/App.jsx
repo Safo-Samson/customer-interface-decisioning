a02e4bd38ef1b78311a290e5a22e49697891b66e
safosamson <safosamson18@gmail.com>
2023-08-08 16:56:14 +0100

    merged branch

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import "./App.css";
 import NewLoan from "./components/HomeLoan/NewLoan";
 import CombinedBar from "./components/UIComponents/CombinedBar";
 
-import { useState } from "react";
-import "./App.css";
-import StartLoan from "./components/startLoan/startLoan.jsx";
 function App() {
   return (
     <>
