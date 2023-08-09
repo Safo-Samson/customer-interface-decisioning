89bc1a392cb196556be6899ed4fbb431493f70ee
safosamson <safosamson18@gmail.com>
2023-08-09 16:52:09 +0100

    minonr

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
-import NewLoan from './components/StartLoan/NewLoan'
+import NewLoan from "./components/StartLoan/NewLoan";
 import CombinedBar from "./components/UIComponents/CombinedBar";
 import StartLoan from "./components/StartLoan/StartLoan";
-import Homepage from "./components/HomeLoan/Homepage";
+import Homepage from "./components/startLoan/Homepage";
 
 function App() {
   return (
