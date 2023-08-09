18c65a12744da6f2c6fadd2bdc404e4b88615c17
safo samson <safosamson18@gmail.com>
2023-08-09 09:38:50 +0100

    plus to buttons

--- src/components/UIComponents/VerticalBar.jsx
+++ src/components/UIComponents/VerticalBar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import "./VerticalBar.css";
 
 const VerticalBar = () => {
@@ -7,18 +8,54 @@ const VerticalBar = () => {
     <div className="vertical-bar">
       <div>
         <div>
-          <button className="vert-button">Our products & services</button>
-          <button className="vert-button">saving & investment</button>
-          <button className="vert-button active">Loans & cars finance</button>
-          <button className="vert-button">bank accounts</button>
-          <button className="vert-button">upgrade bank account</button>
-          <button className="vert-button">credit cards</button>
-          <button className="vert-button">overdrafts</button>
-          <button className="vert-button">insurance</button>
-          <button className="vert-button">pensions</button>
-          <button className="vert-button">mortgages</button>
-          <button className="vert-button">other services</button>
-          <button className="vert-button">others</button>
+          <button className="vert-button">
+            Our products & services{" "}
+            <FontAwesomeIcon icon={faPlus} className="vert-button-icon" />
+          </button>
+          <button className="vert-button">
+            saving & investment{" "}
+            <FontAwesomeIcon icon={faPlus} className="vert-button-icon" />
+          </button>
+          <button className="vert-button active">
+            Loans & cars finance{" "}
+            <FontAwesomeIcon icon={faPlus} className="vert-button-icon" />
+          </button>
+          <button className="vert-button">
+            bank accounts{" "}
+            <FontAwesomeIcon icon={faPlus} className="vert-button-icon" />
+          </button>
+          <button className="vert-button">
+            upgrade bank account{" "}
+            <FontAwesomeIcon icon={faPlus} className="vert-button-icon" />
+          </button>
+          <button className="vert-button">
+            credit cards{" "}
+            <FontAwesomeIcon icon={faPlus} className="vert-button-icon" />
+          </button>
+          <button className="vert-button">
+            overdrafts{" "}
+            <FontAwesomeIcon icon={faPlus} className="vert-button-icon" />
+          </button>
+          <button className="vert-button">
+            insurance{" "}
+            <FontAwesomeIcon icon={faPlus} className="vert-button-icon" />
+          </button>
+          <button className="vert-button">
+            pensions{" "}
+            <FontAwesomeIcon icon={faPlus} className="vert-button-icon" />
+          </button>
+          <button className="vert-button">
+            mortgages{" "}
+            <FontAwesomeIcon icon={faPlus} className="vert-button-icon" />
+          </button>
+          <button className="vert-button">
+            other services{" "}
+            <FontAwesomeIcon icon={faPlus} className="vert-button-icon" />
+          </button>
+          <button className="vert-button">
+            others{" "}
+            <FontAwesomeIcon icon={faPlus} className="vert-button-icon" />
+          </button>
         </div>
       </div>
     </div>
