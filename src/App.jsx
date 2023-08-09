a6b5e5ffbc4d91b484a5793b9b9445c1ca0e3dc3
emiunal <E.unal2@newcastle.ac.uk>
2023-08-09 11:30:37 +0100

    combine vertical bar with infocheck

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import "./App.css";
 import NewLoan from './components/StartLoan/NewLoan'
 import CombinedBar from "./components/UIComponents/CombinedBar";
 import StartLoan from "./components/StartLoan/StartLoan";
+import InformationCheck from './components/InformationCheck/InformationCheck'
+import InfoCombined from './components/InformationCheck/InfoCombined'
 
 function App() {
   return (
     <>
       <CombinedBar />
-      <NewLoan />
-      {/* <StartLoan /> */}
+      <InfoCombined /> 
     </>
   );
 }
