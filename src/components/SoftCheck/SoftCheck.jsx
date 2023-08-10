f79391ec3a56e81504e3856159eda65499931d38
emiunal <E.unal2@newcastle.ac.uk>
2023-08-10 16:36:12 +0100

    style changes

--- src/components/SoftCheck/SoftCheck.jsx
+++ src/components/SoftCheck/SoftCheck.jsx
@@ -25,10 +25,18 @@ const SoftCheck= () =>{
 
                     <div className="softCheck-bad">
                     <FontAwesomeIcon className= 'softCheck-exclamationMarkIcon'icon={faExclamation} />
+                
+                    <p><FontAwesomeIcon icon={faExclamation} style={{background: 'white', width: '10.67px', height:'10.67px',borderRadius: '20px', color: '#481468', padding: '3px', marginRight: '7px'}}/> Electricity bill payment past due</p>
+                    <p><FontAwesomeIcon icon={faExclamation} style={{background: 'white', width: '10.67px', height:'10.67px',borderRadius: '20px', color: '#481468', padding: '3px', marginRight: '7px'}}/> Moved 3 times the past year</p>
 
                     </div>
                     <div className="softCheck-good">
                     <FontAwesomeIcon className= 'softCheck-checkMarkIcon'icon={faCheck} />
+                    <p><FontAwesomeIcon icon={faCheck} style={{background: 'white', width: '10.67px', height:'10.67px',borderRadius: '20px', color: '#481468', padding: '3px', marginRight: '7px'}}/>All debit balances paid off</p>
+                    <p><FontAwesomeIcon icon={faCheck} style={{background: 'white', width: '10.67px', height:'10.67px',borderRadius: '20px', color: '#481468', padding: '3px', marginRight: '7px'}}/>Mix of 4 credit cards used</p>
+                    <p><FontAwesomeIcon icon={faCheck} style={{background: 'white', width: '10.67px', height:'10.67px',borderRadius: '20px', color: '#481468', padding: '3px', marginRight: '7px'}}/>Stable income of the past 5 years</p>
+
+
 
                 </div>
                 </div>
