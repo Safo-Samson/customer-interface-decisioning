e20564f74105780195acc22bd870a0cafcce69e8
safosamson <safosamson18@gmail.com>
2023-08-10 11:50:44 +0100

    adding model decision

---
+++ src/components/startLoan/ModelDecision.jsx
@@ -0,0 +1,13 @@
+import VerticalBar from "../UIComponents/VerticalBar";
+
+const ModelDecision = ({ decision, setDecision }) => {
+  return (
+    <div className="Homepage-container">
+      <VerticalBar />
+
+      <div className="Homepage-remaining-content"></div>
+    </div>
+  );
+};
+
+export default ModelDecision;
