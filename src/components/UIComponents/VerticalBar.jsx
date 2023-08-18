20656e8571a99eedd8a19d95651552adb011d411
safosamson <safosamson18@gmail.com>
2023-08-18 09:47:52 +0100

    vert container fixed

--- src/components/UIComponents/VerticalBar.jsx
+++ src/components/UIComponents/VerticalBar.jsx
@@ -8,11 +8,11 @@ const VerticalBar = () => {
     <div className="vertical-bar">
       <div>
         <div>
-          <button className="vert-button">
+          <button className="vert-button" style={{ paddingLeft: "10%" }}>
             Our products & services{" "}
             <FontAwesomeIcon icon={faPlus} className="vert-button-icon" />
           </button>
-          <button className="vert-button">
+          <button className="vert-button" style={{ paddingLeft: "15%" }}>
             saving & investment{" "}
             <FontAwesomeIcon icon={faPlus} className="vert-button-icon" />
           </button>
@@ -20,47 +20,47 @@ const VerticalBar = () => {
             Loans & cars finance{" "}
             {/* <FontAwesomeIcon icon={faPlus} className="vert-button-icon" /> */}
           </button>
-          <button className="vert-button">
+          <button className="vert-button" style={{ paddingLeft: "25%" }}>
             bank accounts{" "}
             <FontAwesomeIcon icon={faPlus} className="vert-button-icon" />
           </button>
-          <button className="vert-button">
+          <button className="vert-button" style={{ paddingLeft: "10%" }}>
             upgrade bank account{" "}
             <FontAwesomeIcon icon={faPlus} className="vert-button-icon" />
           </button>
-          <button className="vert-button">
+          <button className="vert-button" style={{ paddingLeft: "26%" }}>
             credit cards{" "}
             <FontAwesomeIcon icon={faPlus} className="vert-button-icon" />
           </button>
-          <button className="vert-button">
+          <button className="vert-button" style={{ paddingLeft: "30%" }}>
             overdrafts{" "}
             <FontAwesomeIcon icon={faPlus} className="vert-button-icon" />
           </button>
-          <button className="vert-button">
+          <button className="vert-button" style={{ paddingLeft: "32%" }}>
             insurance{" "}
             <FontAwesomeIcon icon={faPlus} className="vert-button-icon" />
           </button>
-          <button className="vert-button">
+          <button className="vert-button" style={{ paddingLeft: "34%" }}>
             pensions{" "}
             <FontAwesomeIcon icon={faPlus} className="vert-button-icon" />
           </button>
-          <button className="vert-button">
+          <button className="vert-button" style={{ paddingLeft: "30%" }}>
             mortgages{" "}
             <FontAwesomeIcon icon={faPlus} className="vert-button-icon" />
           </button>
-          <button className="vert-button">
+          <button className="vert-button" style={{ paddingLeft: "26%" }}>
             other services{" "}
             <FontAwesomeIcon icon={faPlus} className="vert-button-icon" />
           </button>
-          <button className="vert-button">
+          <button className="vert-button" style={{ paddingLeft: "30%" }}>
             Lifetime ISA
             <FontAwesomeIcon icon={faPlus} className="vert-button-icon" />
           </button>
-          <button className="vert-button">
+          <button className="vert-button" style={{ paddingLeft: "30%" }}>
             Savings ISA
             <FontAwesomeIcon icon={faPlus} className="vert-button-icon" />
           </button>
-          <button className="vert-button">
+          <button className="vert-button" style={{ paddingLeft: "36%" }}>
             others{" "}
             <FontAwesomeIcon icon={faPlus} className="vert-button-icon" />
           </button>
